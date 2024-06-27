41112f83a4533cc715674306040c2f96860e6ed8
HaritaNekkanti <harita.nekkanti@gmail.com>
2024-06-27 09:35:58 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,8 +26,7 @@ if ingredients_list:
   ingredients_string = ''
   for fruit_chosen in ingredients_list:
       ingredients_string += fruit_chosen + ' '
-      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-      fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+      
 
       
    #st.write(ingredients_string)
@@ -46,5 +45,12 @@ if ingredients_list:
 
 
 import requests
+if ingredients_list:
+  ingredients_string = ''
+  for fruit_chosen in ingredients_list:
+      ingredients_string += fruit_chosen + ' '
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+      fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
 
       
