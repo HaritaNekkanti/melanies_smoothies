23ebeb13f74ea13248711f7e010c0cef56bf4b49
HaritaNekkanti <harita.nekkanti@gmail.com>
2024-06-27 12:30:14 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,25 +27,6 @@ ingredients_list = st.multiselect ('Choose up to 5 ingredients:'
 , max_selections=5
 )
 
-if ingredients_list:
-  ingredients_string = ''
-  for fruit_chosen in ingredients_list:
-      ingredients_string += fruit_chosen + ' '
-      
-search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
-
-   #st.write(ingredients_string)
-
-  my_insert_stmt = """ insert into smoothies.public.orders(ingredients,NAME_ON_ORDER)
-            values ('""" + ingredients_string + """','"""+name_on_order+ """')"""
-
-   #st.write(my_insert_stmt)
-  time_to_insert = st.button ('Submit Order')
-
-  if time_to_insert:
-    session.sql(my_insert_stmt).collect()
-    st.success('Your Smoothie is ordered!', icon="✅")
 
 
 import requests
